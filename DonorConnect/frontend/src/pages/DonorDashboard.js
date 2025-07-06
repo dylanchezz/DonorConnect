74c20a0dfc8341ec470f9c5c4060789e256cadac
dylanchezz <saisidylan@gmail.com>
2025-07-06 23:46:19 +0300

    Log out routes and Donor Search bar.

--- DonorConnect/frontend/src/pages/DonorDashboard.js
+++ DonorConnect/frontend/src/pages/DonorDashboard.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import DonorSidebar from './DonorSidebar';
 import { Outlet } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import '../styles/DonorDashboard.css'; 
 const DonorDashboard = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    if (!token || !user || user.role !== 'Donor') {
+      navigate('/login');
+    }
+  }, [navigate]);
+
   return (
-    <div className="dashboard-content">
+    <div className="dashboard-layout">
       <DonorSidebar />
       <main className="dashboard-main">
         <Outlet />
--- DonorConnect/frontend/src/pages/PatientDashboard.js
+++ DonorConnect/frontend/src/pages/PatientDashboard.js
@@ -2,8 +2,20 @@ import React from 'react';
 import PatientSidebar from './PatientSidebar';
 import { Outlet } from 'react-router-dom';
 import '../styles/PatientDashboard.css';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const PatientDashboard = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    if (!token || !user || user.role !== 'Patient') {
+      navigate('/login');
+    }
+  }, [navigate]);
+  
   return (
     <div className="dashboard-content">
       <PatientSidebar />
